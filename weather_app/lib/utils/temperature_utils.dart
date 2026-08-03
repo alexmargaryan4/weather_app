5f54b1e270c78ac6df931ddfbf4cad18349fe8ab
alexmargaryan4 <alex.margaryan4@tumo.org>
2026-08-03 22:31:56 +0400

    Add files via upload

--- weather_app/lib/utils/temperature_utils.dart
+++ weather_app/lib/utils/temperature_utils.dart
@@ -1,1 +1,14 @@
+/// Утилиты для перевода и форматирования температуры.
+/// Все данные из API приходят в градусах Цельсия (units=metric);
+/// эти функции только меняют то, что видит пользователь.
+class TemperatureUtils {
+  static double celsiusToDisplay(double celsius, bool useFahrenheit) {
+    if (!useFahrenheit) return celsius;
+    return celsius * 9 / 5 + 32;
+  }
 
+  static String format(double celsius, bool useFahrenheit) {
+    final value = celsiusToDisplay(celsius, useFahrenheit);
+    return '${value.round()}°';
+  }
+}
